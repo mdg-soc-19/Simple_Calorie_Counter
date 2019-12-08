5387d4b7ce3f176d99abbf60e61ea6014a7f6280
Hardik5247 <54976262+Hardik5247@users.noreply.github.com>
2019-12-08 15:52:01 +0530

    Filling Spinner's of Date and Year

--- app/src/main/java/com/diet/simplecaloriecounter/simplecaloriecounter/SignUp.java
+++ app/src/main/java/com/diet/simplecaloriecounter/simplecaloriecounter/SignUp.java
@@ -1,14 +1,42 @@
 package com.diet.simplecaloriecounter.simplecaloriecounter;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import java.util.Calendar;
+
 public class SignUp extends AppCompatActivity {
+
+    int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+    private String [] arraySpinnerDOBDate = new String[31];
+    private String [] arraySpinnerDOBYear = new String[thisYear - 1899];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        // Filling Spinner of Date//
+
+        for (int x = 0; x < 31; x++){
+            this.arraySpinnerDOBDate[x] = "" + (x + 1);
+        }
+        Spinner spinnerDOBDate = findViewById(R.id.spinnerDOBDate);
+        ArrayAdapter<String> adapterDate = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,arraySpinnerDOBDate);
+        spinnerDOBDate.setAdapter(adapterDate);
+
+        // Filling Spinner of Year//
+        for (int i = thisYear; i >= 1900; i--) {
+            this.arraySpinnerDOBYear[thisYear - i] = "" + i;
+        }
+        Spinner spinnerDOBYear = findViewById(R.id.spinnerDOBYear);
+        ArrayAdapter<String> adapterYear = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arraySpinnerDOBYear);
+        spinnerDOBYear.setAdapter(adapterYear);
+
     }
+
 }
